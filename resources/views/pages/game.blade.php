7db589181ff5d443b7a0070ee05451aa170c98f5
crissavino <csavino@padelmanager.com>
2020-04-24 17:40:10 +0200

    frontend of the game

--- resources/views/pages/game.blade.php
+++ resources/views/pages/game.blade.php
@@ -5,15 +5,8 @@
 @section('main')
 
     <div class="row">
-        <div class="col-sm-2 d-none d-sm-block publicity-left">
-            <div class="publicity-block"></div>
-            <div class="publicity-block"></div>
-            <div class="publicity-block"></div>
-            <div class="publicity-block"></div>
-            <div class="publicity-block"></div>
-        </div>
 
-        <div class="col-8">
+        <div class="col-10">
             <h1 class="text-center d-block game-title">{{ $game->title }}</h1>
             <div class="text-center d-block">
                 @foreach ($game->tags as $tag)
@@ -21,9 +14,13 @@
                 @endforeach
             </div>
 
-            <div class="col-10 m-auto">
+            <div class="col-12 m-auto text-center">
                 <div class="iframe-container d-block mt-5 p-4 m-auto">
                     {!! $game->iframe !!}
+                </div>
+
+                <div class="publicity-horizontal">
+
                 </div>
 
                 <div class="game-description d-block mt-3 mb-3">
@@ -40,14 +37,20 @@
                     </p>
                 </div>
 
+                <div class="publicity-horizontal">
+
+                </div>
+
                 <div class="related-games mt-5">
-                    <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
+                    <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"
+                         data-interval="1000">
                         <div class="MultiCarousel-inner games">
                             @foreach ($relatedGames as $relatedGame)
                                 <div class="item">
                                     <div class="game-box">
                                         <a href="{{route('game', ['id' => $relatedGame->id])}}">
-                                            <img loading="lazy" class="lazyload flex-fill" src="{{asset('images/'.$relatedGame->title.'.png')}}"
+                                            <img loading="lazy" class="lazyload flex-fill"
+                                                 src="{{asset('images/'.$relatedGame->title.'.png')}}"
                                                  alt="">
                                         </a>
                                     </div>
@@ -77,78 +80,70 @@
     $(function() {
       $('.games > div').hover(
           function() {
-            $(this).toggleClass('fadeIn')
+            $(this).toggleClass('fadeIn');
           }).mouseout(function() {
-        $(this).removeClass('fadeIn')
+        $(this).removeClass('fadeIn');
       });
     });
 
     var itemsMainDiv = ('.MultiCarousel');
     var itemsDiv = ('.MultiCarousel-inner');
-    var itemWidth = "";
+    var itemWidth = '';
 
-    $('.leftLst, .rightLst').click(function () {
-      var condition = $(this).hasClass("leftLst");
+    $('.leftLst, .rightLst').click(function() {
+      var condition = $(this).hasClass('leftLst');
       if (condition)
         click(0, this);
       else
-        click(1, this)
+        click(1, this);
     });
 
     ResCarouselSize();
 
-
-
-
-    $(window).resize(function () {
+    $(window).resize(function() {
       ResCarouselSize();
     });
 
     //this function define the size of the items
     function ResCarouselSize() {
       var incno = 0;
-      var dataItems = ("data-items");
+      var dataItems = ('data-items');
       var itemClass = ('.item');
       var id = 0;
       var btnParentSb = '';
       var itemsSplit = '';
       var sampwidth = $(itemsMainDiv).width();
       var bodyWidth = $('body').width();
-      $(itemsDiv).each(function () {
+      $(itemsDiv).each(function() {
         id = id + 1;
         var itemNumbers = $(this).find(itemClass).length;
         btnParentSb = $(this).parent().attr(dataItems);
         itemsSplit = btnParentSb.split(',');
-        $(this).parent().attr("id", "MultiCarousel" + id);
-
+        $(this).parent().attr('id', 'MultiCarousel' + id);
 
         if (bodyWidth >= 1200) {
           incno = itemsSplit[3];
           itemWidth = sampwidth / incno;
-        }
-        else if (bodyWidth >= 992) {
+        } else if (bodyWidth >= 992) {
           incno = itemsSplit[2];
           itemWidth = sampwidth / incno;
-        }
-        else if (bodyWidth >= 768) {
+        } else if (bodyWidth >= 768) {
           incno = itemsSplit[1];
           itemWidth = sampwidth / incno;
-        }
-        else {
+        } else {
           incno = itemsSplit[0];
           itemWidth = sampwidth / incno;
         }
-        $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
-        $(this).find(itemClass).each(function () {
+        $(this).css({'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers});
+        $(this).find(itemClass).each(function() {
           $(this).outerWidth(itemWidth);
         });
 
-        $(".leftLst").addClass("over");
-        $(".rightLst").removeClass("over");
+        $('.leftLst').addClass('over');
+        $('.rightLst').removeClass('over');
 
       });
     }
-
 
     //this function used to move the items
     function ResCarousel(e, el, s) {
@@ -160,21 +155,20 @@
       var xds = Math.abs(values[4]);
       if (e == 0) {
         translateXval = parseInt(xds) - parseInt(itemWidth * s);
-        $(el + ' ' + rightBtn).removeClass("over");
+        $(el + ' ' + rightBtn).removeClass('over');
 
         if (translateXval <= itemWidth / 2) {
           translateXval = 0;
-          $(el + ' ' + leftBtn).addClass("over");
+          $(el + ' ' + leftBtn).addClass('over');
         }
-      }
-      else if (e == 1) {
+      } else if (e == 1) {
         var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
         translateXval = parseInt(xds) + parseInt(itemWidth * s);
-        $(el + ' ' + leftBtn).removeClass("over");
+        $(el + ' ' + leftBtn).removeClass('over');
 
         if (translateXval >= itemsCondition - itemWidth / 2) {
           translateXval = itemsCondition;
-          $(el + ' ' + rightBtn).addClass("over");
+          $(el + ' ' + rightBtn).addClass('over');
         }
       }
       $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
@@ -182,12 +176,15 @@
 
     //It is used to get some elements from btn
     function click(ell, ee) {
-      var Parent = "#" + $(ee).parent().attr("id");
-      var slide = $(Parent).attr("data-slide");
+      var Parent = '#' + $(ee).parent().attr('id');
+      var slide = $(Parent).attr('data-slide');
       ResCarousel(ell, Parent, slide);
     }
 
-
+    // acomodo el iframe
+    let iframe = document.getElementsByTagName('iframe')[0];
+    iframe.style.borderRadius = '10px'
+    //  fin de acomode el iframe
   };
 
   (async () => {
